707612bd75ac597027efe37fdde3379bba95cd7c
Tim Malte Graefje <Tim.Graefje@gmx.de>
2011-11-26 17:11:51 +0100

    Added Main Loop

--- Include/Main.h
+++ Include/Main.h
@@ -7,8 +7,24 @@
 
 #pragma once/* MAIN_H_ */
 
+#include "CommandLine.h"
+
+#include <SDL/SDL.h>
+
 class Main
 {
+private:
+	bool Running;
+
+	SDL_Surface* pDisplay;
+
 public:
 	Main();
+
+	bool OnInit(CL_ParamList* pCL_Params);
+	void OnExit();
+
+	int OnExecute(CL_ParamList* pCL_Params);
+
+	void OnRender();
 };
--- Source/Main.cpp
+++ Source/Main.cpp
@@ -13,15 +13,68 @@
 
 Main::Main()
 {
-	// TODO Auto-generated constructor stub
+	Running = true;
+	pDisplay = NULL;
+}
 
+bool Main::OnInit(CL_ParamList* pCL_Params)
+{
+	Uint16 DisplayW, DisplayH;
+	DisplayW = CheckCL_Param(pCL_Params,"Width").intvalue;
+	DisplayH = CheckCL_Param(pCL_Params,"Height").intvalue;
+
+	if(!DisplayW) DisplayW = 1280;
+	if(!DisplayH) DisplayH = 720;
+
+	if(SDL_Init(SDL_INIT_VIDEO))
+		return false;
+
+	if((pDisplay = SDL_SetVideoMode(DisplayW, DisplayH, 32, SDL_HWSURFACE | SDL_DOUBLEBUF)) == NULL)
+		return false;
+
+	return true;
+}
+
+void Main::OnExit()
+{
+	SDL_FreeSurface(pDisplay);
+
+	SDL_Quit();
+}
+
+void Main::OnRender()
+{
+	SDL_FillRect(pDisplay, NULL, SDL_MapRGB(pDisplay->format, 0,0,0));
+
+	SDL_Flip(pDisplay);
+}
+
+int Main::OnExecute(CL_ParamList* pCL_Params)
+{
+	if(!OnInit(pCL_Params))
+		return -1;
+
+	SDL_Event Event;
+
+	while(Running)
+	{
+		while(SDL_PollEvent(&Event))
+		{
+			if(Event.type == SDL_QUIT)
+				Running = false;
+		}
+		OnRender();
+	}
+
+	OnExit();
+	return 1;
 }
 
 int main(int argc, char** argv)
 {
 	CL_ParamList CL_Params = ParseCommandLine(argc, argv);
-	char* s = CheckCL_Param(&CL_Params, "asdf").stringvalue;
-	std::cout << "JEAS" << std::endl;
-	std::cout << s;
-	return 0;
+	Main App;
+	int ret = App.OnExecute(&CL_Params);
+	DeleteCL_Params(CL_Params.Head);
+	return ret;
 }
